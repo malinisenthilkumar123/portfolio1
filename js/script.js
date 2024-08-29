43b2b1f19473bd340dd1d2bc16fbf86431408e23
malinisenthilkumar123 <malinis576@gmail.com>
2024-08-29 10:35:58 +0530

    Add files via upload
    ok

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// script.js
+
+const toggleButton = document.getElementById('modeToggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        toggleButton.textContent = 'Switch to Light Mode';
+    } else {
+        toggleButton.textContent = 'Switch to Dark Mode';
+    }
+});
